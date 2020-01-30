9a4e3d6b71cb2f3d3eff7d88928edda52769024f
krishna <algotkrish@gmail.com>
2020-01-30 19:14:04 +0530

    dcommit

---
+++ dploy/mysite/app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+
+
+
+
+
+
+def deploy(request):
+	return HttpResponse('first deployment')
